c4c1bd862a00544cb5d29bdbaa1f50dd1a11540f
maryamfaizan53 <maryam.faizanmianoor@gmail.com>
2024-12-31 02:25:10 +0500

    asasd

--- blogwebsite14withsanity/src/app/api/revalidate/route.ts
+++ blogwebsite14withsanity/src/app/api/revalidate/route.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { revalidateTag } from "next/cache";
 import { type NextRequest, NextResponse } from "next/server";
 import { parseBody } from "next-sanity/webhook";
