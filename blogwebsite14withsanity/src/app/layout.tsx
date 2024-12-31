1100ceb8ec766a9b87a0dca595378cc24f266778
maryamfaizan53 <maryam.faizanmianoor@gmail.com>
2025-01-01 02:38:19 +0500

    push hoga

--- blogwebsite14withsanity/src/app/layout.tsx
+++ blogwebsite14withsanity/src/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/app/components/Navbar";
-import ThemeProvider from "@/app/components/ThemeProvider";
-import Footer from "@/app/components/Footer";
-import Container from "@/app/components/Container";
+import Navbar from "../components/Navbar";
+import ThemeProvider from "../components/ThemeProvider";
+import Footer from "../components/Footer";
+import Container from "../components/Container";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Gloss&Grit: Discover the Latest in Web Development and AI",
+  title: "Gloss&Grit: Discover the Latest in Health and Beauty",
   description: "Welcome to our health and beauty blog blog, where we share our insights, opinions, and best practices on health and beauty related content. Here you will find articles on various topics, such as hair care , skin care, top trends, fashion and more. Whether you will find something interesting and valuable on our blog. Check out our latest posts and subscribe to our newsletter to stay updated.",
 };
 
@@ -39,3 +39,7 @@ export default function RootLayout({
     </html>
   );
 }
+
+
+// push kro code koo.han..kro.. mai to bimaar hn...yeh toh pull bol rha haii grabar hogye gi
+//  naya babnah prrega//q...code konsa hai iskp repo kholo 
--- blogwebsite14withsanity/src/components/ThemeProvider.tsx
+++ blogwebsite14withsanity/src/components/ThemeProvider.tsx
@@ -1,8 +1,20 @@
 'use client';
+
 import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { type ThemeProviderProps } from 'next-themes';
+import type { ThemeProviderProps } from 'next-themes';
 
 export default function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
+
+
+// 'use client';
+// import * as React from 'react';
+// import ThemeProviderfrom 'next-theme';
+// import type  ThemeProviderProps  from 'next-themes';
+
+// export default function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+//   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+// }
+
